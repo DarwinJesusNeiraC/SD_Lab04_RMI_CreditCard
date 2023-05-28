2201ad42f60abad0d0597ff4b1d9e1f8afe421ed
dneirac <dneirac@unsa.edu.pe>
2023-05-27 23:00:59 -0500

    Exception para manejar el mal ingreso de datos

---
+++ MyException.java
@@ -0,0 +1,13 @@
+/*
+* @Autor: Darwin Neira Carrasco
+* @Autor: Fabiola Tapara Quispe
+* @Autor: Angel Hincho Jove
+* @Descripcion:
+*/
+
+public class MyException extends Exception{
+  public MyException(String msg){
+    super(msg);
+  }
+}
+
